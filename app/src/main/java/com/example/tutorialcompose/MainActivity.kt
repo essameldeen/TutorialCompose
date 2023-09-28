b4f5b27bcf8aef961fa29e1af7c3cf9e0fa8913b
esammohamed <esammohamed17121996@gmail.com>
2023-09-28 17:22:00 +0300

    bottom sheet with compose

--- app/src/main/java/com/example/tutorialcompose/MainActivity.kt
+++ app/src/main/java/com/example/tutorialcompose/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.tutorialcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.ExperimentalMaterialApi
+import com.example.tutorialcompose.bottom_sheet.ScreenBottomSheet
 import com.example.tutorialcompose.paging.ScreenPaging
 
 import com.example.tutorialcompose.ui.theme.TutorialComposeTheme
-
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TutorialComposeTheme {
-                ScreenPaging()
+                //ScreenPaging()
+                ScreenBottomSheet()
+
+
             }
         }
     }
