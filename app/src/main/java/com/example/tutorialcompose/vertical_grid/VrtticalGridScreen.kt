e2c8fd157e316b3c5cf2b9ae559c6654f2c41538
esammohamed <esammohamed17121996@gmail.com>
2023-10-01 17:03:38 +0300

    add state and animate to specific  index

--- app/src/main/java/com/example/tutorialcompose/vertical_grid/VrtticalGridScreen.kt
+++ app/src/main/java/com/example/tutorialcompose/vertical_grid/VrtticalGridScreen.kt
@@ -6,23 +6,35 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun VerticalGridScreen() {
 
+
+    val scope  = rememberCoroutineScope()
+    val state = rememberLazyGridState(
+        initialFirstVisibleItemIndex = 99
+    ).apply {
+        scope.launch {
+            animateScrollToItem(99,1)
+        }
+
+    }
     LazyVerticalGrid(
         columns = GridCells.Adaptive(100.dp),
-
+        state = state,
         content = {
             items(100) { index ->
 
